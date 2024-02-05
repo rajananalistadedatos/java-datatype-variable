b08011f854047d0e11f9dbcebc99f249d10d1bd3
rajananalistadedatos <141495075+rajananalistadedatos@users.noreply.github.com>
2024-02-05 20:57:53 +0530

    Add files via upload

---
+++ challanges.java
@@ -0,0 +1,5 @@
+public class challanges {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        int myNumber = 456;
+
+        int yourNumber = 234;
+        System.out.println(yourNumber + myNumber);
+        boolean myResult =true;
+        System.out.println(myResult);
+        float myPercentile = 7.22f;
+        System.out.println(myPercentile);
+
+        String wishes ="good night";
+        System.out.println(wishes);
+
+    }
+}
